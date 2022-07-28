aa695280fd178599a0f2edc1d7dbcb3694ef0bb6
raajvir <raajvir@icloud.com>
2022-07-28 15:57:16 +0530

    johnydepp

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
